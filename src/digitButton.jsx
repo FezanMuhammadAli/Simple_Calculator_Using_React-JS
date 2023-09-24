3efb4b489bb099e0e3fd95e5f2554c50a51f463c
Fezan Muhammad Ali <callmetheengcountio@gmail.com>
2023-09-24 11:25:19 +0500

    digitButton.jsx and operationButton.jsx is added with functionlity

---
+++ src/digitButton.jsx
@@ -0,0 +1,11 @@
+import { ACTIONS } from "./App";
+
+export function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
