50113dadc6d9591ae81eb8e9b46da78526634475
Fezan Muhammad Ali <callmetheengcountio@gmail.com>
2023-09-24 07:48:28 +0500

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-// import logo from './logo.svg';
-import "./App.css";
+import "./style.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="privous-operand">123,123</div>
+        <div className="current-operand">123,123</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
 }
 
 export default App;
